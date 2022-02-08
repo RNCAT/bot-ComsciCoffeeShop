aad8aff7056419ee2981b1bb8656723f13e54dd4
RNCAT <jaruwat.works@gmail.com>
2022-02-08 11:46:25 +0700

    :ambulance: add allow headers

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({ allowedHeaders: '*' }))
 
 app.post('/user/:userid', userController.register)
 app.delete('/user/:userid', userController.logout)
